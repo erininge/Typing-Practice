8115140a18204bb82e9a648017eabd3e59b19314
erininge <eriningebritson@gmail.com>
2026-02-16 14:39:33 -0600

    Bump app and service worker cache version to v1.2.22

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker (GitHub Pages-safe, update-friendly)
-const VERSION = "v1.2.21";
+const VERSION = "v1.2.22";
 const CACHE_NAME = `typing-practice-${VERSION}`;
 const PRECACHE_URLS = [
   "./",
